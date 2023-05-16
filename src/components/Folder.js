440ce5b408f8dc65d6ad64309accc1acbc8f8944
harjitsingh3 <singh.harjit09@gmail.com>
2023-05-16 14:43:59 -0400

    Finish Phase 2

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Headers = ({ titles, currentTab, selectTab }) => {
   const handleClick = (e) => {
@@ -28,21 +28,51 @@ const Headers = ({ titles, currentTab, selectTab }) => {
   );
 }
 
-class Folder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentTab: 0
-    };
-  }
+// class oldFolder extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       currentTab: 0
+//     };
+//   }
   
-  selectTab = (num) => {
-    this.setState({ currentTab: num });
-  }
+//   selectTab = (num) => {
+//     this.setState({ currentTab: num });
+//   }
   
-  render() {
-    const folder = this.props.folders[this.state.currentTab];
-    const titles = this.props.folders.map((folder) => folder.title);
+//   render() {
+//     const folder = this.props.folders[this.state.currentTab];
+//     const titles = this.props.folders.map((folder) => folder.title);
+    
+//     return (
+//       <section className="tabs-section">
+//         <h1>Tabs</h1>
+//         <div className='tabs'>
+//           <Headers
+//             titles={titles}
+//             currentTab={this.state.currentTab}
+//             selectTab={this.selectTab}
+//           />
+//           <div className='tab-content'>
+//             {folder.content}
+//           </div>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
+
+function Folder (props) {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  // const selectTab = (num) => {
+  //   // num.preventDefault();
+
+  //   setCurrentTab({currentTab: num})
+  // }
+
+  const folder = props.folders[currentTab];
+    const titles = props.folders.map((folder) => folder.title);
     
     return (
       <section className="tabs-section">
@@ -50,8 +80,8 @@ class Folder extends React.Component {
         <div className='tabs'>
           <Headers
             titles={titles}
-            currentTab={this.state.currentTab}
-            selectTab={this.selectTab}
+            currentTab={currentTab}
+            selectTab={setCurrentTab}
           />
           <div className='tab-content'>
             {folder.content}
@@ -59,7 +89,6 @@ class Folder extends React.Component {
         </div>
       </section>
     );
-  }
 }
 
 export default Folder;
